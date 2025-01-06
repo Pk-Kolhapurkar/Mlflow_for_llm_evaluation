b776e109e56d7d9b2987673ca1c2b13f297e1bf9
Prathamesh Khade <121573184+Pk-Kolhapurkar@users.noreply.github.com>
2025-01-06 01:55:36 +0000

    a

--- load_dataset.py
+++
@@ -1,3 +0,0 @@
-from datasets import load_dataset
-# Load the dataset IMDb for sentiment analysis
-dataset = load_dataset("imdb")
--- preprocessing.py
+++
@@ -1,3 +0,0 @@
-def preprocess_function(examples):
-    return tokenizer(examples['text'], padding="max_length", truncation=True)
-tokenized_datasets = dataset.map(preprocess_function, batched=True)
